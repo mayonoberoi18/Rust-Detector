d7be7ae3370a08146ab7a799e87a695835431f4a
mayonoberoi18 <mayonoberoi18@gmail.com>
2026-04-15 14:33:26 +0530

    Update rust.detector.py

--- rust.detector.py
+++ rust.detector.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Load your HTML file
-with open("your_file.html", "r", encoding="utf-8") as f:
+with open("rust-detector.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
 # Render it in the app
